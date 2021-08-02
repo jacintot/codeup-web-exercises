1855b0d3d25671a0d60520049518dd3a0da46ddb
Jacinto Torres <jacintotorres138@gmail.com>
2021-08-01 21:28:07 -0500

    conditionals.html

--- js/conditionals.js
+++ js/conditionals.js
@@ -173,16 +173,212 @@ alert("Your lucky number was " + luckyNumber + ". Your bill was " + totalBill + 
  * Can you refactor your code to use functions?
  * HINT: The way we prompt for a value could be improved
  */
+//used instructors example to try to figure it out? Still trying to figure it out...
+var likeToEnterNumber = confirm("Would you like to enter a number?");
+    if(likeToEnterNumber) {
+        var userNumber = prompt("Enter a number");
+}
+
+var oddOrEven;
+    if (userNumber % 2 === 0) {
+        oddOrEven = userNumber + " is even.";
+    }else {
+        oddOrEven = userNumber + " is odd";
+    }
+
+alert(oddOrEven);
+
+var numberPlus100 = userNumber + " plus 100 equals " + (Number(userNumber) + 100) + ".";
+
+alert(numberPlus100);
+
+var negativeOrPositive;
+    if (userNumber < 0) {
+        negativeOrPositive = "That is a negative number.";
+    }else {
+        negativeOrPositive = "That is a positive number.";
+    }
+    alert(negativeOrPositive);
+
+
+//Refactor so if what the user enters is not a number, there's an alert.
+// var likeToEnterNumber = confirm("Would you like to enter a number?");
+// if (likeToEnterNumber){
+//   var userNumber = prompt("Enter a number");
+//   if (isNaN(userNumber)){
+//    alert("That is not a number!");
+//   } else {
+//     var oddOrEven;
+//     if (userNumber%2===0){
+//        oddOrEven = userNumber + " is even.";
+//     } else {
+//        oddOrEven = userNumber + " is odd";
+//     }
+
+//     alert(oddOrEven);
+
+//     var numberPlus100 = userNumber + " plus 100 equals " + (Number(userNumber) + 100) + ".";
+
+//     alert(numberPlus100);
+
+//     var negativeOrPositive;
+//     if (userNumber < 0) {
+//        negativeOrPositive = "That is a negative number.";
+//     } else {
+//        negativeOrPositive = "That is a positive number.";
+//     }
+//     alert(negativeOrPositive);
+//       }
+//     }
+
+//Refactor to use functions
+//function created to get number
+// function getNumber() {
+//    var likeToEnterNumber = confirm("Would you like to enter a number?");
+//    if (likeToEnterNumber){
+//        var userNumber = prompt("Enter a number!");
+//        if (isNaN(userNumber)){
+//            alert("That is not a number!");
+//            return NaN;
+//           } else {
+//            userNumber = parseInt(userNumber);
+//            return userNumber;
+//           }
+//    }
+// }
+
+// A function that determines odd or even
+
+// function oddOrEven(number){
+//    number = parseInt(number);
+//    if (number%2==0){
+//        return number + " is even.";
+//    } else {
+//        return number + " is odd."
+//    }
+// }
+
+// A function that determines the number plus 100
+// I have to parse number so it's not a string, and
+// even then I have to wrap the addition in parentheses or it'll concatenate
+
+// function numberPlus100(number){
+//    number = parseInt(number);
+//    return number + " plus 100 equals " + (number + 100) + ".";
+// }
+
+// A function that determines negative and positive
+
+// function negativeOrPositive(number){
+//    number = parseInt(number);
+//    if (number < 0) {
+//        return number + " is a negative number.";
+//     } else {
+//        return number + " is a positive number.";
+//     }
+// }
+
+// Run it all like this:
+
+// var theUsersNumber = getNumber();
+// alert(oddOrEven(theUsersNumber));
+// alert(numberPlus100(theUsersNumber));
+// alert(negativeOrPositive(theUsersNumber));
 
 
 
+// This part is the same
 
+// function getNumber() {
+//     var likeToEnterNumber = confirm("Would you like to enter a number?");
+//     if (likeToEnterNumber){
+//         var userNumber = prompt("Enter a number!");
+//         if (isNaN(userNumber)){
+//             alert("That is not a number!");
+//             return NaN;
+//         } else {
+//             userNumber = parseInt(userNumber);
+//             return userNumber;
+//         }
+//     }
+// }
 
+//Refactored oddOrEven to use ternary operator
+// function oddOrEven(number){
+//     return (number%2===0) ? number + " is even." : number + " is odd.";
+//  }
 
+// I refactor negativeOrPositive to use a ternary operator
 
+// function negativeOrPositive(number){
+//    return (number < 0) ? number + " is a negative number." : number + " is a positive number.";
+// }
 
+// function numberPlus100(number){
+//     number = parseInt(number);
+//     return number + " plus 100 equals " + (number + 100) + ".";
+// }
 
+// I check to see if what I get is a number again, as I did at first
+// I can't do if number === NaN
+// it has to be !isNaN because NaN is never equal to NaN
 
+// var maybeANumber = getNumber();
+// if (!isNaN(maybeANumber)) {
+//     alert(oddOrEven(maybeANumber));
+//     alert(numberPlus100(maybeANumber));
+//     alert(negativeOrPositive(maybeANumber));
+// }
+
+//Final version where everything is a function
+// I start the ball rolling with analyzeNumber
+// analyzeNumber calls getNumber
+// getNumber runs the confirm dialog, and if the user wants,
+// prompts the user and runs returnNaNOrANumber
+// returnNanOrANumber returns either NaN, if the user did not enter a number,
+// or a number if they did enter one
+// since we pass getNumber to analyzeNumber, analyze number
+// can check if the passed value isNaN. If it isn't, it runs
+// the oddOrEven, numberPlus100, and negativeOrPositive functions
+// and it alerts their outputs
+
+function oddOrEven(number){
+    return (number%2===0) ? number + " is even." : number + " is odd.";
+}
+
+function numberPlus100(number){
+    var mySum = number + 100;
+    return number + " plus 100 equals " + mySum + ".";
+}
+
+function negativeOrPositive(number){
+    return (number < 0) ? number + " is a negative number." : number + " is a positive number.";
+}
+
+function getNumber() {
+    if (confirm("Do you want to enter a number?")){
+        return returnNaNOrNumber(prompt("Enter a number!"));
+    }
+}
+
+function returnNaNOrNumber(input){
+    if (isNaN(input)){
+        alert("That is not a number!");
+        return NaN;
+    } else {
+        return parseInt(input);
+    }
+}
+
+function analyzeNumber(input){
+    if (!isNaN(input)){
+        alert(oddOrEven(input));
+        alert(numberPlus100(input));
+        alert(negativeOrPositive(input));
+    }
+}
+
+analyzeNumber(getNumber());
 
 
 
