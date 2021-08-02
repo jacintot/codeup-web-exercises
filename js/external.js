5d03b42d9d2cab05be216bab940341ca3882b622
Jacinto Torres <jacintotorres138@gmail.com>
2021-08-01 22:26:25 -0500

    external_js.html

---
+++ js/external.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//var littleMermaid = 3
+//var brotherBear  = 5
+//var hercules = 1
+//var rental = 3
+//   alert((littleMermaid + brotherBear + hercules) * rental);
+
+
+//var googlePay = 400
+//var amazonPay = 380
+//var faceBookPay = 350
+//var googleHours = 6
+//var amazonHours = 4
+//var faceBookHours = 10
+//alert((googlePay * googleHours) + (amazonPay * amazonHours) + (faceBookPay * faceBookHours))
+
+//var classIsFull = false;
+//var scheduleConflict = false;
+//var classCapacity = 25;
+//function canEnroll(currentStudents, doesConflict) {
+//    return currentStudents < classCapacity && !doesConflict;
+//}
+//canEnroll(10, false)
+
+
+
+//A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+//var discountTwoOrMore = 2;
+//var numberOfItems = prompt("How many items were bought?");
+//var offerIsNotExpired = confirm("Confirm offer has not expired.");
+//var isAPremiumMember = confirm("Customer is a premium member");
+//var productDiscountIsApplied = offerIsNotExpired  && (isAPremiumMember || numberOfItems >= discountTwoOrMore);
+
+ //alert("Product discount applied: " + productDiscountIsApplied);
